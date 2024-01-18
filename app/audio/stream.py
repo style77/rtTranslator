ce63e3f85250443f0d280bc744c44210616eea97
style77 <stylek777@gmail.com>
2024-01-18 12:26:12 +0100

    chore: custom implementation of sr.Microphone

---
+++ app/audio/stream.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+
+class Speakers(sr.Microphone):
+    @staticmethod
+    def get_pyaudio():
+        """
+        Imports the pyaudio module and checks its version. Throws exceptions if pyaudio can't be found or a wrong version is installed
+        """
+        try:
+            import pyaudiowpatch as pyaudio
+        except ImportError:
+            raise AttributeError("Could not find PyAudio; check installation")
+        from distutils.version import LooseVersion
+        if LooseVersion(pyaudio.__version__) < LooseVersion("0.2.11"):
+            raise AttributeError("PyAudio 0.2.11 or later is required (found version {})".format(pyaudio.__version__))
+        return pyaudio
